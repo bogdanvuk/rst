8c53644dec80157ad5701cec1fba32910c9a6dbf
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2016-07-12 01:16:16 +0200

    Ranking for various efti settings

--- examples/doktorat/source/scripts/cv_efti_rank.py
+++ examples/doktorat/source/scripts/cv_efti_rank.py
@@ -26,7 +26,7 @@ def combine_efti_data(data, efti_res, attr_name, factor):
         ds = r['dataset']
         if 'EFTI' not in data[attr_name][ds]:
             data[attr_name][ds]['EFTI'] = []
-            
+
         data[attr_name][ds]['EFTI'].append(r[attr_name]*factor)
 
 def load_other_algo_data(cv_other_dir):
@@ -39,47 +39,109 @@ def load_other_algo_data(cv_other_dir):
 def load_cv_data(cv_other_dir, efti_cvjs):
     data = load_other_algo_data(cv_other_dir)
     efti_res = load_js_data(efti_cvjs)
-    
+
     for attr_name, factor in zip(['accuracy', 'leaves'], [100, 1]):
         combine_efti_data(data, efti_res, attr_name, factor)
 
     return data
 
-efti_cvjs = 'cv_20160708_131037_pc_impurity.js'
-results_acc_csv = 'cv_20160708_131037_pc_impurity_acc.csv'
-results_size_csv = 'cv_20160708_131037_pc_impurity_size.csv'
-cv_other_dir = '/home/bvukobratovic/projects/rst/examples/doktorat/source/scripts/'
-data = load_cv_data(cv_other_dir, efti_cvjs)
-
-acc_table = {}
-size_table = {}
-for ds in data['accuracy']:
-    acc_table[ds] = {}
-    size_table[ds] = {}
-    for algo in algos:
-        acc_table[ds][algo] = mean_confidence_interval(data['accuracy'][ds][algo])
-        size_table[ds][algo] = mean_confidence_interval(data['leaves'][ds][algo])
-
-for fn, table in zip([results_acc_csv, results_size_csv], [acc_table, size_table]):
+def form_csv_table(fn, table, algos):
     with open(fn, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',',
                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    
+
         csvwriter.writerow(['Dataset'] + algos)
-    
+
         for d,res in iter(sorted(table.items())):
             row = [d]
-        
+
             for a in algos:
                 #row += [":math:`{0:0.1f} \pm {1:0.2f}`".format(res[a][0], res[a][1])]
-                row += ["{0:0.2f}±{1:0.2f}".format(res[a][0], res[a][1])]
-               
+                #row += ["{0:0.2f}±{1:0.2f}".format(res[a][0], res[a][1])]
+                row += ["{0:0.2f}".format(res[a][0])]
+
             csvwriter.writerow(row)
-        
-    # row = ['Average speedup', '']
-    
-    # row += [":math:`{0:0.1f} \pm {1:0.2f}`".format(*mean_confidence_interval(spdup_arm))]
-    # row += [":math:`{0:0.1f} \pm {1:0.2f}`".format(*mean_confidence_interval(spdup_pc))]
-    # row += [":math:`{0:0.1f} \pm {1:0.2f}`".format(*mean_confidence_interval(spdup_dsp))]
-    
-        #csvwriter.writerow(row)
+
+        # row = ['Average speedup', '']
+
+        # row += [":math:`{0:0.1f} \pm {1:0.2f}`".format(*mean_confidence_interval(spdup_arm))]
+        # row += [":math:`{0:0.1f} \pm {1:0.2f}`".format(*mean_confidence_interval(spdup_pc))]
+        # row += [":math:`{0:0.1f} \pm {1:0.2f}`".format(*mean_confidence_interval(spdup_dsp))]
+
+            #csvwriter.writerow(row)
+
+def efti_with_others():
+    efti_cvjs = 'cv_20160708_131037_pc_impurity.js'
+    results_acc_csv = 'cv_20160708_131037_pc_impurity_acc.csv'
+    results_size_csv = 'cv_20160708_131037_pc_impurity_size.csv'
+    cv_other_dir = '/home/bvukobratovic/projects/rst/examples/doktorat/source/scripts/'
+    data = load_cv_data(cv_other_dir, efti_cvjs)
+
+    acc_table = {}
+    size_table = {}
+    for ds in data['accuracy']:
+        acc_table[ds] = {}
+        size_table[ds] = {}
+        for algo in algos:
+            acc_table[ds][algo] = mean_confidence_interval(data['accuracy'][ds][algo])
+            size_table[ds][algo] = mean_confidence_interval(data['leaves'][ds][algo])
+
+    for fn, table in zip([results_acc_csv, results_size_csv], [acc_table, size_table]):
+        form_csv_table(fn, table, algos)
+
+def combine_efti_data(data, efti_res, attr_name, factor):
+    for r in efti_res['pc_run']:
+        ds = r['dataset']
+        if 'EFTI' not in data[attr_name][ds]:
+            data[attr_name][ds]['EFTI'] = []
+
+        data[attr_name][ds]['EFTI'].append(r[attr_name]*factor)
+
+def inter_efti():
+    efti_cvjs = [
+        'cv_20160709_153106_pc_impurity.js',
+        'cv_20160709_153150_pc_impurity.js',
+        'cv_20160709_153152_pc_impurity.js',
+        'cv_20160709_153155_pc_impurity.js',
+        'cv_20160711_231443_pc_impurity.js',
+        'cv_20160711_231457_pc_impurity.js',
+        'cv_20160711_231509_pc_impurity.js',
+        'cv_20160711_231519_pc_impurity.js'
+    ]
+
+    data = {'accuracy': {}, 'size': {}}
+    for i, e in enumerate(efti_cvjs):
+        efti_res = load_js_data(e)
+        for r in efti_res['pc_run']:
+            ds = r['dataset']
+
+            if ds not in data['accuracy']:
+                data['accuracy'][ds] = []
+                data['size'][ds] = []
+
+            if i not in data['accuracy'][ds]:
+                data['accuracy'][ds].extend([[] for _ in range(1 + i - len(data['accuracy'][ds]))])
+                data['size'][ds].extend([[] for _ in range(1 + i - len(data['size'][ds]))])
+
+            data['size'][ds][i].append(r['leaves'])
+            data['accuracy'][ds][i].append(r['accuracy'])
+
+    acc_table = {}
+    size_table = {}
+    for ds in data['accuracy']:
+        acc_table[ds] = {}
+        size_table[ds] = {}
+        for algo in range(len(efti_cvjs)):
+            if data['accuracy'][ds][algo]:
+                acc_table[ds][algo] = mean_confidence_interval(data['accuracy'][ds][algo])
+                size_table[ds][algo] = mean_confidence_interval(data['size'][ds][algo])
+            else:
+                acc_table[ds][algo] = (0,0)
+                size_table[ds][algo] = (0,0)
+
+    results_acc_csv = 'efti_compare_acc.csv'
+    results_size_csv = 'efti_compare_size.csv'
+    for fn, table in zip([results_acc_csv, results_size_csv], [acc_table, size_table]):
+        form_csv_table(fn, table, list(range(len(efti_cvjs))))
+
+inter_efti()
--- examples/doktorat/source/scripts/cv_pc.py
+++ examples/doktorat/source/scripts/cv_pc.py
@@ -1,14 +1,12 @@
 from subprocess import Popen, PIPE
 from cv_server import SerialCmd
-from multicomp_rank import multicomp_rank
+#from multicomp_rank import multicomp_rank
 import time
 import json
 import pexpect
 import os
+import sys
 
-algos = ['OC1-AP', 'OC1', 'CART-LC', 'OC1-SA', 'OC1-GA', 'OC1-ES', 'GALE', 'GaTree', 'HB-Lin', 'EFTI']
-
-efti_app = '/home/bvukobratovic/personal/doktorat/prj/efti_pc/Release/efti_pc --max_iter=1000000'
 
 def crossvalidation(fname):
     with SerialCmd(fname) as ser_cmd:
@@ -21,7 +19,7 @@ def crossvalidation(fname):
                 ser_cmd.cmd_decode(p.before + '\n')
         except pexpect.EOF:
             pass
-    
+
 def load_js_data(fname):
     with open(fname) as data_file:
         res = json.load(data_file)
@@ -34,7 +32,7 @@ def rank(data, desc=False):
         #if ds not in ['w40', 'vow']:
         #if ds not in ['gls', 'ion', 'page', 'son', 'veh', 'vow', 'w40', 'zoo']:
         ranks[ds] = multicomp_rank(data[ds], desc=desc)
-        
+
     return ranks
 
 def combine_efti_data(data, efti_res, attr_name, factor):
@@ -42,7 +40,7 @@ def combine_efti_data(data, efti_res, attr_name, factor):
         ds = r['dataset']
         if 'EFTI' not in data[attr_name][ds]:
             data[attr_name][ds]['EFTI'] = []
-            
+
         data[attr_name][ds]['EFTI'].append(r[attr_name]*factor)
 
 def load_other_algo_data(cv_other_dir):
@@ -55,7 +53,7 @@ def load_other_algo_data(cv_other_dir):
 def load_cv_data(cv_other_dir, efti_cvjs):
     data = load_other_algo_data(cv_other_dir)
     efti_res = load_js_data(efti_cvjs)
-    
+
     for attr_name, factor in zip(['accuracy', 'leaves'], [100, 1]):
         combine_efti_data(data, efti_res, attr_name, factor)
 
@@ -78,18 +76,18 @@ def create_rank_table(data, desc, rankjs):
                     combined[a] = []
 
                 combined[a].append(r);
-        
+
         avg[attr_name] = {}
         for a in combined:
             avg[attr_name][a] = sum(combined[a])/len(combined[a])
-    
+
 
         print(sorted(avg[attr_name].items(), key=lambda x: x[1]))
 
     with open(rankjs, 'w') as outfile:
         json.dump({'rank': algo_rank, 'avg': avg}, outfile)
 
-categ_max = {'ausc': 2,'bc': 3,'bcw': 2,'ger': 2,'gls': 7,'hep': 2,'hrts': 2,'ion': 2,'irs': 3,'liv': 2,'lym': 4,'page': 5,'pid': 2,'son': 2,'thy': 4,'veh': 4,'vote': 2,'vow': 11,'w40': 3,'zoo': 7} 
+categ_max = {'ausc': 2,'bc': 3,'bcw': 2,'ger': 2,'gls': 7,'hep': 2,'hrts': 2,'ion': 2,'irs': 3,'liv': 2,'lym': 4,'page': 5,'pid': 2,'son': 2,'thy': 4,'veh': 4,'vote': 2,'vow': 11,'w40': 3,'zoo': 7}
 
 def calc_data_fitness(data, complexity_weight=0.05):
     fit_data = {}
@@ -99,20 +97,27 @@ def calc_data_fitness(data, complexity_weight=0.05):
             fit_data[ds][algo] = []
             for acc, leaves in zip(data['accuracy'][ds][algo], data['leaves'][ds][algo]):
                 fit_data[ds][algo].append(acc * (complexity_weight*(categ_max[ds] - leaves)/categ_max[ds] + 1));
-                
-    return {'fit': fit_data}            
 
-#efti_cvjs = "cv_{0}_pc_impurity.js".format(time.strftime("%Y%m%d_%H%M%S"))
-#rankjs = "cv_{0}_pc_impurity_rank.js".format(time.strftime("%Y%m%d_%H%M%S"))
-cv_other_dir = '/home/bvukobratovic/projects/rst/examples/doktorat/source/scripts/'
-efti_cvjs = 'cv_20160708_161726_pc_impurity.js'
-rankjs = 'cv_20160708_161726_pc_impurity_rank.js'
+    return {'fit': fit_data}
 
-crossvalidation(efti_cvjs)
-data = load_cv_data(cv_other_dir, efti_cvjs)
+if __name__ == "__main__":
 
-fit_data = calc_data_fitness(data)
-create_rank_table(fit_data, [True], rankjs)
+   algos = ['OC1-AP', 'OC1', 'CART-LC', 'OC1-SA', 'OC1-GA', 'OC1-ES', 'GALE', 'GaTree', 'HB-Lin', 'EFTI']
+
+   efti_app = '/home/projects/efti/Release/efti ' + ' '.join(sys.argv[1:])
+
+
+   efti_cvjs = "cv_{0}_pc_impurity.js".format(time.strftime("%Y%m%d_%H%M%S"))
+   rankjs = "cv_{0}_pc_impurity_rank.js".format(time.strftime("%Y%m%d_%H%M%S"))
+   cv_other_dir = '.'
+   #efti_cvjs = 'cv_20160708_161726_pc_impurity.js'
+   #rankjs = 'cv_20160708_161726_pc_impurity_rank.js'
+
+   crossvalidation(efti_cvjs)
+#data = load_cv_data(cv_other_dir, efti_cvjs)
+
+#fit_data = calc_data_fitness(data)
+#create_rank_table(fit_data, [True], rankjs)
 
 #create_rank_table(data, [True, False], rankjs)
 
